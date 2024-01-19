0e673803a599fe6e225de561c596a42ed68e1d3a
Gautam <gautam.shirodkar92@gmail.com>
2024-01-19 11:05:11 +0530

    fix(deployment-bugs): added supporting files for vitest globals

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), AutoImport({
+    imports: ['vitest'],
+    dts: true, // generate TypeScript declaration
+  }),],
   test: {
     globals: true,
     environment: 'jsdom',
